4a5ae6ef8f55caa2df9104b6af00a4b0068ec273
Fsmart2164 <24fs2381@barton.ac.uk>
2025-04-24 13:26:46 +0100

    Add project files.

---
+++ Game v2/Area.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_v2
+{
+    public class Map
+    {
+        List<Area> map;
+        public Map()
+        {
+
+        }
+    }
+
+    public class Area
+    {
+        List<Coord> cordlist;
+        public Area(List<Coord> coords)
+        {
+            cordlist = new List<Coord>(coords);
+        }
+        public void printmap()
+        {
+            foreach (Coord c in cordlist)
+            {
+                Console.CursorTop = c.gety();
+                Console.CursorLeft = c.getx();
+            }
+        }
+
+    }
+    public class Coord
+    {
+        protected int x; protected int y;
+        public Coord(int inx, int iny)
+        {
+            x = inx;
+            y = iny;
+        }
+        public int getx()
+        {
+            return x;
+        }
+        public int gety()
+        {
+            return y;
+        }
+        public virtual bool isedge()
+        {
+            return false;
+        }
+    }
+    public class edge : Coord
+    {
+         public edge(int inx, int iny) : base(inx, iny)
+        {
+        }
+    }
+
+    public class Gateway:edge 
+    {
+        public Gateway(int inx, int iny) : base(inx, iny)
+        {
+
+        }
+    }
+}
+
---
+++ Game v2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.CursorTop = 10;
+            Console.CursorLeft = 10;
+        }
+    }
+}
