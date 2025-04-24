7ee0032ff446b2744c5d62492a719f11337eed38
Fsmart2164 <24fs2381@barton.ac.uk>
2025-04-24 13:26:46 +0100

    Add project files.

---
+++ Game v2/Area.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game_v2
+{
+    public class Map
+    {
+        List<Area> map;
+        public Map()
+        {
+
+        }
+    }
+
+    public class Area
+    {
+        List<Coord> cordlist;
+        public Area(List<Coord> coords)
+        {
+            cordlist = new List<Coord>(coords);
+        }
+        public void printmap()
+        {
+            foreach (Coord c in cordlist)
+            {
+
+                Console.CursorTop = c.getx();
+                Console.CursorLeft = c.gety();
+                switch (c.getedgetype())
+                {
+                    case "top":
+                        Console.CursorTop--;
+                        Console.Write("-");
+                        Console.CursorTop++;
+                        Console.CursorLeft--;
+                        break;
+                    case "left":
+                        Console.CursorLeft--;
+                        Console.Write("|");
+                        Console.CursorLeft--;
+                        Console.CursorLeft--;
+                        break;
+                    case "right":
+                        Console.CursorLeft++;
+                        Console.Write("|");
+                        break;
+                    case "bottom":
+                        Console.CursorTop++;
+                        Console.Write("-");
+                        Console.CursorLeft--;
+                        break;
+                    case "topleft":
+                        Console.CursorTop--;
+                        Console.Write("-");
+                        Console.CursorTop++;
+                        Console.CursorLeft--;
+                        Console.CursorLeft--;
+                        Console.Write("|");
+                        Console.CursorLeft--;
+                        break;
+                    case "topright":
+                        Console.CursorTop--;
+                        Console.Write("-");
+                        Console.CursorTop++;
+                        Console.Write('|');
+                        Console.CursorLeft--;
+                        break;
+                    case "bottomleft":
+                        Console.CursorTop++;
+                        Console.Write("-");
+                        Console.CursorTop--;
+                        Console.CursorLeft--;
+                        Console.CursorLeft--;
+                        Console.Write('|');
+                        Console.CursorLeft--;
+                        break;
+                    case "bottomright":
+                        Console.CursorTop++;
+                        Console.Write("-");
+                        Console.CursorTop--;
+                        Console.Write('|');
+                        break;
+
+                }
+            }
+        }
+
+    }
+    public class Coord
+    {
+        protected int x; protected int y;
+        public Coord(int inx, int iny)
+        {
+            x = inx;
+            y = iny;
+        }
+        public int getx()
+        {
+            return x;
+        }
+        public int gety()
+        {
+            return y;
+        }
+
+        public virtual string getedgetype()
+        {
+            return "none";
+        }
+    }
+    public class edge : Coord
+    {
+        protected string edgetype;
+        public edge(int inx, int iny, string edgetype) : base(inx, iny)
+        {
+            this.edgetype = edgetype;
+        }
+
+        public override string getedgetype()
+        {
+            return edgetype;
+        }
+    }
+
+    public class Gateway : edge
+    {
+        Area[] linked;
+        public Gateway(int inx, int iny, Area[] linked, string edgetype) : base(inx, iny, edgetype)
+        {
+            this.linked = linked;
+        }
+    }
+}
+
---
+++ Game v2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            List<Coord> coordList = new List<Coord>()
+            {
+                new edge(11,11,"topleft"),
+                new edge(11,12,"top"),
+                new edge (11,13,"top"),
+                new edge(11,14,"topright"),
+
+                new edge(12,11,"left"),
+                new Coord(12,12),
+                new Coord(12,13),
+                new edge(12,14,"right"),
+
+                new edge(13,11,"bottomleft"),
+                new edge(13,12,"bottom"),    
+                new edge(13, 13,"bottom"),
+                new edge(13,14,"bottomright"),
+            };
+            Area start = new Area(coordList);
+            start.printmap();
+            Console.CursorTop = 10;
+            Console.CursorLeft = 10;
+            Console.ReadKey(true);
+            Console.CursorTop--;
+            Console.Write("-");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
